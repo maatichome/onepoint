92720d96115c5647d661c8bf87a1bf177e4689a1
sony <sony@192.168.0.8>
2018-01-29 15:00:23 -0500

    some changes

---
+++ src/main/java/com/onepoint/app/dao/LoginDao.java
@@ -0,0 +1,8 @@
+package com.onepoint.app.dao;
+
+import com.onepoint.app.model.Login;
+
+public interface LoginDao {
+	void validateLogin(Login login);
+
+}
---
+++ src/main/java/com/onepoint/app/dao/LoginDaoImpl.java
@@ -0,0 +1,14 @@
+package com.onepoint.app.dao;
+
+import com.onepoint.app.model.Login;
+
+public class LoginDaoImpl implements LoginDao{
+
+	@Override
+	public void validateLogin(Login login) {
+		
+		
+	}
+	
+
+}
